dd879817f8cf3b3ad1ebe0636a9b12b80fee7372
Joe_Siu <www.2000joe@gmail.com>
2021-11-28 17:08:39 +0800

    Added card fade in animation

---
+++ firebase.js
@@ -0,0 +1,8 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyD3UyYTi6GvbrM6P05EtGfRHHjKwq3J6Jw",
+    authDomain: "botpulation.firebaseapp.com",
+    projectId: "botpulation",
+  });
+  
+  var db = firebase.firestore();
--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyD3UyYTi6GvbrM6P05EtGfRHHjKwq3J6Jw",
-  authDomain: "botpulation.firebaseapp.com",
-  projectId: "botpulation",
-});
-
-var db = firebase.firestore();
 var data;
-var prevDataLength = 0;
+var currentDataIndex = 0;
+var createCardAction;
 
 function init() {
   fetchData();
 
-  setInterval(fetchData, 5000);
+  // setInterval(fetchData, 5000);
 }
 
 function fetchData() {
@@ -21,24 +14,25 @@ function fetchData() {
     .get()
     .then((querySnapshot) => {
       data = querySnapshot.docs.map((doc) => Object.assign(doc.data(), { id: doc.id }));
-      
-      if (prevDataLength != data.length) {
-        createCard();
-        prevDataLength = data.length;
+
+      if (currentDataIndex != data.length) {
+        createCardAction = setInterval(createCard, 100);
       }
     });
 }
 
 function createCard() {
-  console.log("Create card...");
-  var parent = document.querySelector(".content");
+  if (currentDataIndex >= data.length - 1) {
+    clearInterval(createCardAction);
+  } else {
+    console.log("Create card...");
+    var parent = document.querySelector(".content");
 
-  for (let index = prevDataLength; index < data.length; index++) {
-    const element = data[index];
+    const element = data[currentDataIndex];
     // Create a card
     var card = document.createElement("div");
-    card.className = "card";
-    card.textContent = `#${index + 1} | `;
+    card.className = "card animate__animated animate__fadeInDown";
+    card.textContent = `#${currentDataIndex + 1} | `;
     card.textContent += `comment: ${element["comment"]} | `;
     card.textContent += `type: ${element["type"]} | `;
     card.textContent += `rating: ${element["rating"]}`;
@@ -46,6 +40,8 @@ function createCard() {
     // Put the card into parent at start
     parent.prepend(card);
     console.log(element);
+
+    currentDataIndex++;
   }
 }
 
