deca030be0d604a5c36fde506fe3570d640f2a10
Joe_Siu <www.2000joe@gmail.com>
2021-11-28 16:34:05 +0800

    Added basic fetching and create card

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "./node_modules/firebase/app";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
   apiKey: "AIzaSyD3UyYTi6GvbrM6P05EtGfRHHjKwq3J6Jw",
   authDomain: "botpulation.firebaseapp.com",
-  databaseURL: "https://botpulation-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "botpulation",
-  storageBucket: "botpulation.appspot.com",
-  messagingSenderId: "880503563342",
-  appId: "1:880503563342:web:d9d18e25f26887b21c0f1f",
-  measurementId: "G-M9YPK5D6MX"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+var db = firebase.firestore();
+var data;
+var prevDataLength = 0;
 
-// Get a reference to the database service
-const database = getDatabase(app);
+function init() {
+  fetchData();
 
-// Read data
-database.ref('data').on('value',(snap)=>{
-    console.log(snap.val());
-  });
+  setInterval(fetchData, 5000);
+}
+
+function fetchData() {
+  console.log("Start fetching...");
+  db.collection("feedback")
+    .get()
+    .then((querySnapshot) => {
+      data = querySnapshot.docs.map((doc) => Object.assign(doc.data(), { id: doc.id }));
+      
+      if (prevDataLength != data.length) {
+        createCard();
+        prevDataLength = data.length;
+      }
+    });
+}
+
+function createCard() {
+  console.log("Create card...");
+  var parent = document.querySelector(".content");
+
+  for (let index = prevDataLength; index < data.length; index++) {
+    const element = data[index];
+    // Create a card
+    var card = document.createElement("div");
+    card.className = "card";
+    card.textContent = `#${index + 1} | `;
+    card.textContent += `comment: ${element["comment"]} | `;
+    card.textContent += `type: ${element["type"]} | `;
+    card.textContent += `rating: ${element["rating"]}`;
+
+    // Put the card into parent at start
+    parent.prepend(card);
+    console.log(element);
+  }
+}
+
+window.onload = init();
